46453e015595b663f06c4fdf507f998c754123cd
jayantt23 <jayant.sharma@iiitb.ac.in>
2025-12-18 19:29:09 +0530

    Create Task entity

---
+++ backend/src/main/java/com/jay/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.jay.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
---
+++ backend/src/main/java/com/jay/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.jay.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN, CLOSED
+}
