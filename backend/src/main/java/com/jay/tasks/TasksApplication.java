60b82838f6b74888fa4ca76696c83adf0799d242
jayantt23 <jayant.sharma@iiitb.ac.in>
2025-12-18 18:19:41 +0530

    Set up connection to the database

--- backend/src/main/java/com/jay/tasks/TasksApplication.java
+++ backend/src/main/java/com/jay/tasks/TasksApplication.java
@@ -1,12 +1,16 @@
 package com.jay.tasks;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class TasksApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
