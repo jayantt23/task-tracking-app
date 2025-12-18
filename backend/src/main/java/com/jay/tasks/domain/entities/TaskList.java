e9dad1f5fddf5163f303c5be10d16bcc0c51b96f
jayantt23 <jayant.sharma@iiitb.ac.in>
2025-12-18 19:36:58 +0530

    Create TaskList entity

---
+++ backend/src/main/java/com/jay/tasks/domain/entities/TaskList.java
@@ -0,0 +1,35 @@
+package com.jay.tasks.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "task_lists")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+
+    @Column(name = "updated", nullable = false)
+    private LocalDateTime updated;
+
+}
